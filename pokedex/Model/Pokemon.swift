7e2a8c03811c49828e87248ffe05f8acee4acb19
Andy Vainauskas <ajvainauskas@gmail.com>
2017-10-02 23:16:46 -0700

    Added Pokemon class and base of collectionView methods

---
+++ pokedex/Model/Pokemon.swift
@@ -0,0 +1,28 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Andy Vainauskas on 10/2/17.
+//  Copyright © 2017 Andy Vainauskas. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
